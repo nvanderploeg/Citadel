1f6a4bbba033684298b6f5f9366ad17fcb819ba3
Norman van der Ploeg <godraisty@gmail.com>
2022-11-08 19:14:19 -0500

    fix inputRouter event binding

--- source/citadel/input/inputRouter.cpp
+++ source/citadel/input/inputRouter.cpp
@@ -12,8 +12,10 @@ namespace citadel
 
     void InputRouter::BindToGLFW(GLFWwindow* window )
     {
+        glfwSetWindowUserPointer(window, this);
+        
         auto lambda = [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-            std::cout << "Key Callback" << std::endl << key << "," << scancode << "," << action << "," << mods << std::endl;
+            std::cout << "Key Callback" << std::endl << key << "," << scancode << "," << action << "," << mods << std::endl << window << std::endl;
             auto self = static_cast<InputRouter*>(glfwGetWindowUserPointer(window));
             auto itr = self->keyInputMap.find(key);
             if (itr != self->keyInputMap.end())
@@ -26,7 +28,7 @@ namespace citadel
         glfwSetKeyCallback(window, lambda);
 
         glfwSetCursorPosCallback(window, [](GLFWwindow* window, double x, double y) {
-            std::cout << "MousePos Callback" << std::endl << "(" << x << ", " << y << ")" << std::endl;
+            // std::cout << "MousePos Callback" << std::endl << "(" << x << ", " << y << ")" << std::endl;
         });
 
         glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
